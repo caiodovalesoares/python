699ea46264de111c638f9ed6f771e5bdfec685ad
CaiodoValeSoares <caiodovalesoares@gmail.com>
2024-11-11 14:17:44 -0300

    Update lista01_15_caiosoares.py

--- lab-uff/lista01/lista01_15_caiosoares.py
+++ lab-uff/lista01/lista01_15_caiosoares.py
@@ -2,3 +2,35 @@
 # Data: 02/11/2024
 # Programa que analisa a altura de mulheres inscritas num concurso de beleza e depois escreve as duas maiores alturas e quantas mulheres as possuem
 
+nome = str(input('Digite o nome da participante: '))
+print('(MARIA encerra o programa)')
+
+maiorAltura = 0
+segMaiorAltura = 0
+contMaior = 0
+contSeg = 0
+while nome != "MARIA" or nome != "maria":
+    altura = float(input('Digite a altura da participante: '))
+    if altura > maiorAltura:
+        maiorAltura = altura
+    if altura < maiorAltura and altura > segMaiorAltura:
+        segMaiorAltura = altura
+    if altura == maiorAltura:
+        contMaior += 1
+    if altura == segMaiorAltura:
+        contSeg += 1
+    nome = str(input('Digite o nome da participante: '))
+    print('(MARIA encerra o programa)')
+else:
+    altura = float(input('Digite a altura da participante: '))
+    if altura > maiorAltura:
+        maiorAltura = altura
+    if altura < maiorAltura and altura > segMaiorAltura:
+        segMaiorAltura = altura
+    if altura == maiorAltura:
+        contMaior += 1
+    if altura == segMaiorAltura:
+        contSeg += 1
+
+print('Maior altura: ', contMaior, 'mulheres com ', maiorAltura)
+print('Segunda maior altura: ', contSeg, 'mulheres com ', segMaiorAltura)
